4bfa9700d34e11d1a5e0d17621df0812c69e4f7c
khairul anuar <ka86101@gmail.com>
2023-05-17 00:02:39 +0800

    first commit

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        // dd(Listing::where('beds', '>=', 4)->orderBy('price', 'asc')->first());
+        return inertia(
+            'Index/Index',
+            [
+                'message' => 'Hello from Laravel!'
+            ]
+        );
+    }
+
+    public function show()
+    {
+        return inertia('Index/Show');
+    }
+}
---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <Link href="/listing">Listings</Link>&nbsp;
+  <Link href="/listing/create">New Listing</Link>
+  <!-- <div>The page with time {{ timer }}</div> -->
+  <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
+  <slot>Default</slot>
+</template>
+  
+<script setup>
+import { computed } from 'vue'
+import { Link, usePage } from '@inertiajs/vue3'
+
+// page.props.value.flash.success
+const flashSuccess = computed(
+  () => usePage().props.flash.success,
+)
+
+// import { ref } from 'vue'
+
+// const timer = ref(0)
+// setInterval(() => timer.value++, 1000)
+</script> 
+
+<style scoped>
+.success {
+  background-color: green;
+  color: white;
+}
+</style>
